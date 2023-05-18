74bf5eb84c1a4028d86add33e3614ea6738da0f4
김나영` <knyg99@naver.com>
2023-05-18 22:21:47 +0900

    식당,숙박

--- app/src/main/java/com/example/jjeck/MainActivity.kt
+++ app/src/main/java/com/example/jjeck/MainActivity.kt
@@ -103,18 +103,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         btn.setOnClickListener {
             getLastLocation()
         }
-        val spinner: Spinner = findViewById(R.id.spinner)
-// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.category,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            spinner.adapter = adapter
-        }
+
     }
 
     // Services such as getLastLocation()
